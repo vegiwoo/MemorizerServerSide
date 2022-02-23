a21c7b3a3127c0295c62dac6b4b967aeafac10ae
vegiwoo <vegiwoo@protonmail.com>
2022-02-24 02:35:26 +0300

    Configuring work with MEMORIZER_JWKS_KEY

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "MemorizerServerSide",
     platforms: [
-        .macOS(.v11)
+        .macOS(.v12)
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -16,41 +16,42 @@ public func configure(_ app: Application) throws {
     
     // MARK: JWT config
     // Request from environment and register a JWKS key to start server
-    guard let jwrsString = Environment.process.JWKS else {
-        app.logger.critical("JWKS key not provided to start server"); fatalError()
+    let MEMORIZER_JWKS_KEY: String = "MEMORIZER_JWKS"
+    guard let jwksString = Environment.get("\(MEMORIZER_JWKS_KEY)") else {
+        app.logger.critical("No value for key '\(MEMORIZER_JWKS_KEY)' was found in environment"); fatalError()
     }
-    try app.jwt.signers.use(jwksJSON: jwrsString)
+    try app.jwt.signers.use(jwksJSON: jwksString)
     
     // MARK: DB config
     /*
      * To register test and prod DB, server at startup expects PSQL_TEST and
      * PSQL_PROD type strings:
-     * 'postgres://ar_collab_db_user:UnbXtxfM88nuEhbvLk0L@127.0.0.1:5432/service_users'
+     * 'postgres://[DB_USERNAME]:[DB_PASSWORD]]@[DB_HOST]:[DB_PORT]/[DB_NAME]'
      */
-    var psqlUrl : String = ""
-    if app.environment.name == "testing" {
-        guard let url = Environment.process.PSQL_TEST else {
-            let error = "No value for key 'PSQL_TEST' was found in environment"
-            throw Abort (HTTPResponseStatus.internalServerError, reason: error)
-        }
-        psqlUrl = url
-        
-    } else {
-        guard let url = Environment.process.PSQL_PROD else {
-            let error = "No value for key 'PSQL_PROD' was found in environment."
-            app.logger.critical("\(error)")
-            throw Abort (HTTPResponseStatus.internalServerError, reason: error)
-        }
-        psqlUrl = url
-    }
-    
-    // Check if string is valid and register DB
-    guard let url = URL(string: psqlUrl) else {
-        let error = "Can`t parse correctly \(psqlUrl)."
-        app.logger.critical("\(error)")
-        throw Abort (HTTPResponseStatus.internalServerError, reason: error)
-      }
-    app.databases.use(try .postgres(url: url), as: .psql)
+//    var psqlUrl : String = ""
+//    if app.environment.name == "testing" {
+//        guard let url = Environment.process.MEMORIZER_PSQL_TEST else {
+//            let error = "No value for key 'MEMORIZER_PSQL_TEST' was found in environment"
+//            throw Abort (HTTPResponseStatus.internalServerError, reason: error)
+//        }
+//        psqlUrl = url
+//
+//    } else {
+//        guard let url = Environment.process.MEMORIZER_PSQL_PROD else {
+//            let error = "No value for key 'MEMORIZER_PSQL_PROD' was found in environment."
+//            app.logger.critical("\(error)")
+//            throw Abort (HTTPResponseStatus.internalServerError, reason: error)
+//        }
+//        psqlUrl = url
+//    }
+//
+//    // Check if string is valid and register DB
+//    guard let url = URL(string: psqlUrl) else {
+//        let error = "Can`t parse correctly \(psqlUrl)."
+//        app.logger.critical("\(error)")
+//        throw Abort (HTTPResponseStatus.internalServerError, reason: error)
+//      }
+//    app.databases.use(try .postgres(url: url), as: .psql)
 
     // Add migrations in DB
 //    if app.environment.name == "testing" {
